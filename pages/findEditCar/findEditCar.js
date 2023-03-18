2d2ce1ee4366e01a090f7888185e5a77d18010ec
stkea <stef240p@stud.kea.dk>
2023-03-18 10:44:16 +0100

    fix

--- pages/findEditCar/findEditCar.js
+++ pages/findEditCar/findEditCar.js
@@ -5,86 +5,101 @@ import { handleHttpErrors } from "../../utils.js";
 const URL = `${API_URL}/cars`;
 
 const handleFetch = async (url, options = undefined) => {
-    return await fetch(url, options).then(handleHttpErrors)
-}
+  return await fetch(url, options).then(handleHttpErrors);
+};
 
 const populateEditForm = (data) => {
-    document.getElementById("car-id").value = data.id
-    document.getElementById("brand").value = data.brand
-    document.getElementById("model").value = data.model
-    document.getElementById("pricePrDay").value = data.pricePrDay
-    document.getElementById("bestDiscount").value = data.bestDiscount
-}
+  document.getElementById("car-id").value = data.id;
+  document.getElementById("brand").value = data.brand;
+  document.getElementById("model").value = data.model;
+  document.getElementById("pricePrDay").value = data.pricePrDay;
+  document.getElementById("bestDiscount").value = data.bestDiscount;
+};
 
 const fetchAndPopulateCar = async () => {
-    const idField = document.getElementById("car-id-input")
+  const idField = document.getElementById("car-id-input");
+  const token = localStorage.getItem("token");
+  if (!token) return;
 
-    if (idField.value === "") return
-    try {
-        const data = await handleFetch(URL + `/admin/${idField.value}`)
-        populateEditForm(data)
-        document.getElementById("fetch-status").innerText = "Car fetched and fields populated"
-    } catch (err) {
-        document.getElementById("fetch-status").innerText = err.message
-    }
-
-}
+  if (idField.value === "") return;
+  try {
+    const data = await handleFetch(URL + `/admin/${idField.value}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    populateEditForm(data);
+    document.getElementById("fetch-status").innerText =
+      "Car fetched and fields populated";
+  } catch (err) {
+    document.getElementById("fetch-status").innerText = err.message;
+  }
+};
 
 const handleSubmitChange = async () => {
-    const id = document.getElementById("car-id")
-    if (id.value === "") return
-    const brand = document.getElementById("brand")
-    const model = document.getElementById("model")
-    const pricePrDay = document.getElementById("pricePrDay")
-    const bestDiscount = document.getElementById("bestDiscount")
-    const body = {
-        id: id.value,
-        brand: brand.value,
-        model: model.value,
-        pricePrDay: pricePrDay.value,
-        bestDiscount: bestDiscount.value
-    }
-    const status = document.getElementById("status")
-    try {
-        await handleFetch(URL, {
-            method: "PUT",
-            body: JSON.stringify(body),
-            headers: {
-                "Content-type": "application/json"
-            }
-        })
-        status.innerText = "Successfully updated car"
-    } catch (err) {
-        status.innerText = err.message
-    }
-}
+  const id = document.getElementById("car-id");
+  const token = localStorage.getItem("token");
+  if (!token) return;
+  if (id.value === "") return;
+  const brand = document.getElementById("brand");
+  const model = document.getElementById("model");
+  const pricePrDay = document.getElementById("pricePrDay");
+  const bestDiscount = document.getElementById("bestDiscount");
+  const body = {
+    id: id.value,
+    brand: brand.value,
+    model: model.value,
+    pricePrDay: pricePrDay.value,
+    bestDiscount: bestDiscount.value,
+  };
+  const status = document.getElementById("status");
+  try {
+    await handleFetch(URL, {
+      method: "PUT",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    status.innerText = "Successfully updated car";
+  } catch (err) {
+    status.innerText = err.message;
+  }
+};
 
 const handleDeleteCar = async () => {
-    const id = document.getElementById("car-id")
-    if (id.value === "") return
-    const status = document.getElementById("status")
-    try {
-        fetch(URL + `/${id.value}`, {
-            method: "DELETE",
-            headers: {
-                "Content-type": "application/json"
-            }
-        })
-        populateEditForm({
-            id: "",
-            brand: "",
-            model: "",
-            pricePrDay: "",
-            bestDiscount: ""
-        })
-        status.innerText = "Successfully deleted car!"
-    } catch (err) {
-        status.innerText = err.message
-    }
-}
+  const id = document.getElementById("car-id");
+  const token = localStorage.getItem("token");
+  if (!token) return;
+  if (id.value === "") return;
+  const status = document.getElementById("status");
+  try {
+    fetch(URL + `/${id.value}`, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+        Authentication: `Bearer ${token}`,
+      },
+    });
+    populateEditForm({
+      id: "",
+      brand: "",
+      model: "",
+      pricePrDay: "",
+      bestDiscount: "",
+    });
+    status.innerText = "Successfully deleted car!";
+  } catch (err) {
+    status.innerText = err.message;
+  }
+};
 
 export function initFindEditCar() {
-    document.getElementById("btn-fetch-car").onclick = async () => await fetchAndPopulateCar()
-    document.getElementById("btn-submit-edited-car").onclick = async () => await handleSubmitChange()
-    document.getElementById("btn-delete-car").onclick = async () => await handleDeleteCar()
+  document.getElementById("btn-fetch-car").onclick = async () =>
+    await fetchAndPopulateCar();
+  document.getElementById("btn-submit-edited-car").onclick = async () =>
+    await handleSubmitChange();
+  document.getElementById("btn-delete-car").onclick = async () =>
+    await handleDeleteCar();
 }
